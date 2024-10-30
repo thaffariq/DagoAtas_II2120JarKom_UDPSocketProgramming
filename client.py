5f94599d409ddbefab4577f49c4f977e1fe9f09b
thaffariq <19623002@std.stei.itb.ac.id>
2024-10-31 06:55:45 +0700

    feat: implemented unique username, ip and port validation, variables as constant

--- client.py
+++ client.py
@@ -1,10 +1,21 @@
 import threading
 import socket
 
+BUFFER_SIZE = 4096
+
+def is_valid_ip(ip):
+    parts = ip.split(".")
+    if len(parts) == 4 and all(part.isdigit() and 0 <= int(part) <= 255 for part in parts):
+        return True
+    return False
+
+def is_valid_port(port):
+    return port.isdigit() and 1024 <= int(port) <= 65535
+
 def recv_messages(client_socket):
     while True:
         try:
-            message, _ = client_socket.recvfrom(4096)
+            message, _ = client_socket.recvfrom(BUFFER_SIZE)
             print(message.decode())
         except:
             print("Disconnected from server.")
@@ -12,22 +23,38 @@ def recv_messages(client_socket):
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = ("localhost", 2305)
+    
+    while True:
+        server_ip = input("Enter server IP: ")
+        server_port = input("Enter server port: ")
+        
+        if is_valid_ip(server_ip) and is_valid_port(server_port):
+            server_address = (server_ip, int(server_port))
+            break
+        else:
+            print("Invalid IP or port. Please try again.")
 
-    username = input("Enter username: ")
+    while True:
+        username = input("Enter username: ")
+        client_socket.sendto(f"USERNAME:{username}".encode(), server_address)
+        
+        response, _ = client_socket.recvfrom(BUFFER_SIZE)
+        response_message = response.decode()
+        print(response_message)
+        
+        if response_message == "Username accepted.":
+            break
 
     while True:
         password = input("Enter password: ")
-        client_socket.sendto(password.encode(), server_address)
+        client_socket.sendto(f"PASSWORD:{password}".encode(), server_address)
         
-        response, _ = client_socket.recvfrom(4096)
+        response, _ = client_socket.recvfrom(BUFFER_SIZE)
         response_message = response.decode()
         print(response_message)
 
         if response_message == "Password accepted. You are connected.":
             break
-
-    client_socket.sendto(f"{username} has joined.".encode(), server_address)
 
     threading.Thread(target=recv_messages, args=(client_socket,)).start()
 
--- server.py
+++ server.py
@@ -1,20 +1,9 @@
-import threading
 import socket
 
+SERVER_IP = "192.168.29.1"
+SERVER_PORT = 9999
 SERVER_PASSWORD = "123"
-
-def handle_client(server_socket, client_address, clients):
-    while True:
-        try:
-            message, address = server_socket.recvfrom(4096)
-            decoded_message = message.decode()
-            print(f"Message from {address}: {decoded_message}")
-            broadcast(server_socket, message, address, clients)
-        except:
-            print(f"Client {client_address} disconnected.")
-            if client_address in clients:
-                del clients[client_address]
-            break
+BUFFER_SIZE = 4096
 
 def broadcast(server_socket, message, source_address, clients):
     for address in clients:
@@ -23,27 +12,38 @@ def broadcast(server_socket, message, source_address, clients):
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(("localhost", 2305))
-    print("Server is running on port 2305")
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+    print(f"Server is running on {SERVER_IP}:{SERVER_PORT}")
 
     clients = {}
+    usernames = {}
 
     while True:
-        message, client_address = server_socket.recvfrom(4096)
+        message, client_address = server_socket.recvfrom(BUFFER_SIZE)
         decoded_message = message.decode()
 
-        if client_address in clients:
-            print(f"Message from {client_address}: {decoded_message}")
-            broadcast(server_socket, message, client_address, clients)
+        if decoded_message.startswith("USERNAME:"):
+            username = decoded_message.split(":", 1)[1]
+            if username in usernames.values():
+                server_socket.sendto("Username already taken. Please choose a different one.".encode(), client_address)
+            else:
+                usernames[client_address] = username
+                server_socket.sendto("Username accepted.".encode(), client_address)
             continue
-        
-        if decoded_message == SERVER_PASSWORD:
-            clients[client_address] = True
-            print(f"Client {client_address} connected.")
-            server_socket.sendto("Password accepted. You are connected.".encode(), client_address)
-        else:
-            print(f"Client {client_address} provided invalid password.")
-            server_socket.sendto("Invalid password. Please try again.".encode(), client_address)
+
+        elif decoded_message.startswith("PASSWORD:"):
+            password = decoded_message.split(":", 1)[1]
+            if password == SERVER_PASSWORD:
+                clients[client_address] = True
+                print(f"Client {client_address} connected with username '{usernames[client_address]}'.")
+                server_socket.sendto("Password accepted. You are connected.".encode(), client_address)
+            else:
+                server_socket.sendto("Invalid password. Please try again.".encode(), client_address)
+            continue
+
+        if client_address in clients:
+            print(f"Message from {client_address} {decoded_message}")
+            broadcast(server_socket, message, client_address, clients)
 
 if __name__ == "__main__":
     main()
