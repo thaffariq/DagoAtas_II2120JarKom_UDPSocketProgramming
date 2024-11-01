3b4c5442ad7201c604ffc026d4ef24eba5b3ff0b
thaffariq <19623002@std.stei.itb.ac.id>
2024-11-01 16:12:40 +0700

    feat: implemented object-oriented programming

--- client.py
+++ client.py
@@ -1,96 +1,105 @@
-import threading
 import socket
+import threading
 
 BUFFER_SIZE = 4096
 KEY_RC4 = "my_secure_key"
 
-def rc4_encrypt_decrypt(key, data):
-    S = list(range(256))
-    j = 0
-    out = []
+class RC4Cipher:
+    def __init__(self, key):
+        self.key = key
 
-    for i in range(256):
-        j = (j + S[i] + ord(key[i % len(key)])) % 256
-        S[i], S[j] = S[j], S[i]
+    def encrypt_decrypt(self, data):
+        S = list(range(256))
+        j = 0
+        out = []
 
-    i = j = 0
-    for char in data:
-        i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        S[i], S[j] = S[j], S[i]
-        out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
+        for i in range(256):
+            j = (j + S[i] + ord(self.key[i % len(self.key)])) % 256
+            S[i], S[j] = S[j], S[i]
 
-    return ''.join(out)
+        i = j = 0
+        for char in data:
+            i = (i + 1) % 256
+            j = (j + S[i]) % 256
+            S[i], S[j] = S[j], S[i]
+            out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
 
-def calculate_checksum(data):
-    return sum(ord(char) for char in data) % 256
+        return ''.join(out)
 
-def is_valid_ip(ip):
-    parts = ip.split(".")
-    return len(parts) == 4 and all(part.isdigit() and 0 <= int(part) <= 255 for part in parts)
+class Client:
+    def __init__(self, buffer_size, key):
+        self.buffer_size = buffer_size
+        self.cipher = RC4Cipher(key)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def is_valid_port(port):
-    return port.isdigit() and 1024 <= int(port) <= 65535
+    def calculate_checksum(self, data):
+        return sum(ord(char) for char in data) % 256
 
-def recv_messages(client_socket):
-    while True:
-        try:
-            message, _ = client_socket.recvfrom(BUFFER_SIZE)
-            decrypted_message = rc4_encrypt_decrypt(KEY_RC4, message.decode())
-            print(decrypted_message)  # Menampilkan pesan tanpa checksum
-        except:
-            print("Terputus dari server.")
-            break
+    def is_valid_ip(self, ip):
+        parts = ip.split(".")
+        return len(parts) == 4 and all(part.isdigit() and 0 <= int(part) <= 255 for part in parts)
 
-def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    while True:
-        server_ip = input("Masukkan IP server: ")
-        server_port = input("Masukkan port server: ")
-        
-        if is_valid_ip(server_ip) and is_valid_port(server_port):
-            server_address = (server_ip, int(server_port))
-            break
-        else:
-            print("IP atau port tidak valid. Silakan coba lagi.")
+    def is_valid_port(self, port):
+        return port.isdigit() and 1024 <= int(port) <= 65535
 
-    while True:
-        username = input("Masukkan username: ")
-        encrypted_username = rc4_encrypt_decrypt(KEY_RC4, f"USERNAME:{username}")
-        client_socket.sendto(encrypted_username.encode(), server_address)
-        
-        response, _ = client_socket.recvfrom(BUFFER_SIZE)
-        decrypted_response = rc4_encrypt_decrypt(KEY_RC4, response.decode())
-        print(decrypted_response)
-        
-        if decrypted_response == "Username diterima.":
-            break
+    def recv_messages(self):
+        while True:
+            try:
+                message, _ = self.client_socket.recvfrom(self.buffer_size)
+                decrypted_message = self.cipher.encrypt_decrypt(message.decode())
+                print(decrypted_message)
+            except:
+                print("Terputus dari server.")
+                break
 
-    while True:
-        password = input("Masukkan password: ")
-        encrypted_password = rc4_encrypt_decrypt(KEY_RC4, f"PASSWORD:{password}")
-        client_socket.sendto(encrypted_password.encode(), server_address)
-        
-        response, _ = client_socket.recvfrom(BUFFER_SIZE)
-        decrypted_response = rc4_encrypt_decrypt(KEY_RC4, response.decode())
-        print(decrypted_response)
+    def connect_to_server(self):
+        while True:
+            server_ip = input("Masukkan IP server: ")
+            server_port = input("Masukkan port server: ")
 
-        if decrypted_response == "Password diterima. Anda terhubung.":
-            break
+            if self.is_valid_ip(server_ip) and self.is_valid_port(server_port):
+                self.server_address = (server_ip, int(server_port))
+                break
+            else:
+                print("IP atau port tidak valid. Silakan coba lagi.")
 
-    threading.Thread(target=recv_messages, args=(client_socket,)).start()
+        while True:
+            username = input("Masukkan username: ")
+            encrypted_username = self.cipher.encrypt_decrypt(f"USERNAME:{username}")
+            self.client_socket.sendto(encrypted_username.encode(), self.server_address)
 
-    while True:
-        message = input()
-        if message.lower() == "exit":
-            print("Anda telah meninggalkan ruang obrolan.")
-            break
+            response, _ = self.client_socket.recvfrom(self.buffer_size)
+            decrypted_response = self.cipher.encrypt_decrypt(response.decode())
+            print(decrypted_response)
 
-        checksum = calculate_checksum(message)
-        complete_message = f"{message}:{checksum}"
-        encrypted_message = rc4_encrypt_decrypt(KEY_RC4, complete_message)
-        client_socket.sendto(encrypted_message.encode(), server_address)
+            if decrypted_response == "Username diterima.":
+                break
+
+        while True:
+            password = input("Masukkan password: ")
+            encrypted_password = self.cipher.encrypt_decrypt(f"PASSWORD:{password}")
+            self.client_socket.sendto(encrypted_password.encode(), self.server_address)
+
+            response, _ = self.client_socket.recvfrom(self.buffer_size)
+            decrypted_response = self.cipher.encrypt_decrypt(response.decode())
+            print(decrypted_response)
+
+            if decrypted_response == "Password diterima. Anda terhubung.":
+                break
+
+        threading.Thread(target=self.recv_messages).start()
+
+        while True:
+            message = input()
+            if message.lower() == "exit":
+                print("Anda telah meninggalkan ruang obrolan.")
+                break
+
+            checksum = self.calculate_checksum(message)
+            complete_message = f"{message}:{checksum}"
+            encrypted_message = self.cipher.encrypt_decrypt(complete_message)
+            self.client_socket.sendto(encrypted_message.encode(), self.server_address)
 
 if __name__ == "__main__":
-    main()
+    client = Client(BUFFER_SIZE, KEY_RC4)
+    client.connect_to_server()
--- server.py
+++ server.py
@@ -1,98 +1,108 @@
 import socket
 import threading
 
-# Mendapatkan alamat IP server secara otomatis
+# Konfigurasi server
 SERVER_IP = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 9999
 SERVER_PASSWORD = "123"
 BUFFER_SIZE = 4096
 KEY_RC4 = "my_secure_key"
 
-def rc4_encrypt_decrypt(key, data):
-    S = list(range(256))
-    j = 0
-    out = []
+class RC4Cipher:
+    def __init__(self, key):
+        self.key = key
 
-    for i in range(256):
-        j = (j + S[i] + ord(key[i % len(key)])) % 256
-        S[i], S[j] = S[j], S[i]
+    def encrypt_decrypt(self, data):
+        S = list(range(256))
+        j = 0
+        out = []
 
-    i = j = 0
-    for char in data:
-        i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        S[i], S[j] = S[j], S[i]
-        out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
+        for i in range(256):
+            j = (j + S[i] + ord(self.key[i % len(self.key)])) % 256
+            S[i], S[j] = S[j], S[i]
 
-    return ''.join(out)
+        i = j = 0
+        for char in data:
+            i = (i + 1) % 256
+            j = (j + S[i]) % 256
+            S[i], S[j] = S[j], S[i]
+            out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
 
-def calculate_checksum(data):
-    return sum(ord(char) for char in data) % 256
+        return ''.join(out)
 
-def broadcast(server_socket, message, source_address, clients):
-    for address in clients:
-        if address != source_address:
-            server_socket.sendto(message, address)
+class Server:
+    def __init__(self, ip, port, password):
+        self.ip = ip
+        self.port = port
+        self.password = password
+        self.clients = {}
+        self.usernames = {}
+        self.cipher = RC4Cipher(KEY_RC4)
 
-def main():
-    # Membuat socket UDP untuk server
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((SERVER_IP, SERVER_PORT))
-    print(f"Server berjalan di {SERVER_IP}:{SERVER_PORT}")
+    def calculate_checksum(self, data):
+        return sum(ord(char) for char in data) % 256
 
-    clients = {}
-    usernames = {}
+    def broadcast(self, message, source_address):
+        for address in self.clients:
+            if address != source_address:
+                self.server_socket.sendto(message, address)
 
-    while True:
-        message, client_address = server_socket.recvfrom(BUFFER_SIZE)
-        decrypted_message = rc4_encrypt_decrypt(KEY_RC4, message.decode())
-        
+    def handle_message(self, message, client_address):
+        decrypted_message = self.cipher.encrypt_decrypt(message.decode())
+
         if decrypted_message.startswith("USERNAME:"):
             username = decrypted_message.split(":", 1)[1]
-            if username in usernames.values():
-                response = rc4_encrypt_decrypt(KEY_RC4, "Username sudah dipakai. Silakan pilih username lain.")
-                server_socket.sendto(response.encode(), client_address)
+            if username in self.usernames.values():
+                response = self.cipher.encrypt_decrypt("Username sudah dipakai. Silakan pilih username lain.")
+                self.server_socket.sendto(response.encode(), client_address)
             else:
-                usernames[client_address] = username
-                response = rc4_encrypt_decrypt(KEY_RC4, "Username diterima.")
-                server_socket.sendto(response.encode(), client_address)
-            continue
+                self.usernames[client_address] = username
+                response = self.cipher.encrypt_decrypt("Username diterima.")
+                self.server_socket.sendto(response.encode(), client_address)
+            return
 
         elif decrypted_message.startswith("PASSWORD:"):
             password = decrypted_message.split(":", 1)[1]
-            if password == SERVER_PASSWORD:
-                clients[client_address] = True
-                print(f"Klien {client_address} terhubung dengan username '{usernames[client_address]}'.")
-                response = rc4_encrypt_decrypt(KEY_RC4, "Password diterima. Anda terhubung.")
-                server_socket.sendto(response.encode(), client_address)
+            if password == self.password:
+                self.clients[client_address] = True
+                print(f"Klien {client_address} terhubung dengan username '{self.usernames[client_address]}'.")
+                response = self.cipher.encrypt_decrypt("Password diterima. Anda terhubung.")
+                self.server_socket.sendto(response.encode(), client_address)
             else:
-                response = rc4_encrypt_decrypt(KEY_RC4, "Password salah. Silakan coba lagi.")
-                server_socket.sendto(response.encode(), client_address)
-            continue
+                response = self.cipher.encrypt_decrypt("Password salah. Silakan coba lagi.")
+                self.server_socket.sendto(response.encode(), client_address)
+            return
 
-        if client_address in clients:
+        if client_address in self.clients:
             try:
-                # Memisahkan data dan checksum yang diterima
                 data, received_checksum = decrypted_message.rsplit(":", 1)
-                calculated_checksum = calculate_checksum(data)
-                
-                # Menampilkan pesan, username, dan checksum di server
+                calculated_checksum = self.calculate_checksum(data)
+
                 if calculated_checksum == int(received_checksum):
-                    username = usernames[client_address]
+                    username = self.usernames[client_address]
                     print(f"Pesan dari {client_address} {username}: '{data}' dengan checksum valid: {received_checksum}")
-                    
-                    # Mempersiapkan pesan untuk dikirim ke klien lain tanpa checksum
+
                     message_to_send = f"{username}: {data}"
-                    encrypted_message = rc4_encrypt_decrypt(KEY_RC4, message_to_send)
-                    broadcast(server_socket, encrypted_message.encode(), client_address, clients)
+                    encrypted_message = self.cipher.encrypt_decrypt(message_to_send)
+                    self.broadcast(encrypted_message.encode(), client_address)
                 else:
                     print("Pesan rusak: checksum tidak sesuai.")
-                    response = rc4_encrypt_decrypt(KEY_RC4, "Pesan rusak: checksum tidak sesuai.")
-                    server_socket.sendto(response.encode(), client_address)
+                    response = self.cipher.encrypt_decrypt("Pesan rusak: checksum tidak sesuai.")
+                    self.server_socket.sendto(response.encode(), client_address)
             except ValueError:
                 print("Format pesan tidak valid.")
-                response = rc4_encrypt_decrypt(KEY_RC4, "Format pesan tidak valid.")
-                server_socket.sendto(response.encode(), client_address)
+                response = self.cipher.encrypt_decrypt("Format pesan tidak valid.")
+                self.server_socket.sendto(response.encode(), client_address)
+
+    def run(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind((self.ip, self.port))
+        print(f"Server berjalan di {self.ip}:{self.port}")
+
+        while True:
+            message, client_address = self.server_socket.recvfrom(BUFFER_SIZE)
+            self.handle_message(message, client_address)
 
 if __name__ == "__main__":
-    main()
+    server = Server(SERVER_IP, SERVER_PORT, SERVER_PASSWORD)
+    server.run()
